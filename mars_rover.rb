46ba9f48f3eeffa6a52ca5c497215fc18e0e33e2
Fernando Ikeda <f_ikeda10@hotmail.com>
2015-06-11 17:41:57 -0400

    mars rover exercise finished

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,36 +1,80 @@
 class Rover
-	POSSIBLE_DIRECTIONS = ["N", "W", "S", "E"]
-	MOVE_STEPS = { 
-		N: [0, 1],
-		W: [-1, 0],
-		S: [0, -1],
-		E: [1, 0]
+	
+	attr_reader :x, :y, :direction
 
+	DIRECTION_VECTORS = { 
+		N: [0,1],
+		W: [-1,0],
+		S: [0,-1],
+		E: [1,0]
 	}
 
-	def initialize(x_coordinate, y_coordinate, direction)
-		@x_coordinate = x_coordinate
-		@y_coordinate = y_coordinate
+	def initialize(x, y, direction, width=5, height=5)
+		@x = x
+		@y = y
 		@direction = direction
-	end
-	
-	def read_instruction
-	end
-	
-	def move(mov)
-
-	end
-	
-	def turn
+		@width, @height = width, height
 	end
 
-	def display 
-		puts "#{@x_coordinate}, #{@y_coordinate}, #{@direction}"
+	def move
+	case @direction
+		when "N"
+			@y += 1
+		when "S"
+			@y -= 1
+		when "E"
+			@x += 1	
+		when "W"
+			@x -= 1
+	end
+	end
+	
+	def turn_left
+      case @direction
+        when "N" 
+        	@direction = "W"
+        when "E" 
+        	@direction = "N"
+        when "S" 
+        	@direction = "E"
+        when "W" 
+        	@direction = "S"
+      end
+    end
+
+    def turn_right
+      case @direction
+        when "N" 
+        	@direction = "E"
+        when "E" 
+        	@direction = "S"
+        when "S" 
+        	@direction = "W"
+        when "W" 
+        	@direction = "N"
+      end
+    end
+
+	def read_instruction(i)
+		case i
+		when "M"
+			move
+		when "L"
+			turn_left
+		when "R"
+			turn_right
+		end
+		puts self
+	end
+
+	def to_s
+		"<Rover: #{x},#{y} facing #{direction}>"
 	end
 end
 
-rover = Rover.new(0, 0, " ")
-rover.display
 
+r1 = Rover.new(1, 2, "N", 5, 5)
+input = "LMLMLMLMM"
+input.each_char {|c| r1.read_instruction(c)}
 
 
--- salestax.rb
+++ salestax.rb
@@ -1,11 +1,22 @@
-class Tax 
-	def Salestax(product)
-		
-		if product = "books"
 
-		end
+class Goods
+	def initialize(product, product_category, price)
+		@product = product
+		@product_category = product_category
+		@price = price
+	end
+	def Salestax(product_category)
+		if product_category == "books" 
+			puts "No sales tax"
 
-		elsif product = "food"
+		elsif product_category == "food"
+			puts "No sales tax"
+
+		elsif product_category == "medical products"
+			puts "No sales tax"
+
+		else
+			puts "Sales tax is $#{price.to_f * 0.10}"
 		end
 	end
 
@@ -17,4 +28,4 @@ product = gets.chomp
 puts "What is this product´s category?"
 product_category = gets.chomp
 puts "What´s the product´s price?"
-
+price = gets.chomp
